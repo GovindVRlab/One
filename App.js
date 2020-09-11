5e53030d25675afb36a3d8d80ca1fbfb03a87ff3
Govind kalsa <govind.kalsa@vrlabtech.com>
2020-09-11 20:50:57 +0530

    created project

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import First from './src/screens/First'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    First : {
+      screen : First,
+      navigationOptions :{
+        header : null
+    }},
   },
-});
+  {
+    initialRouteName: 'First', 
+  }
+);
+
+
+export default createAppContainer(navigator);
+ 
---
+++ src/screens/First.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const First = () => {
+    return (
+        <View>
+            <Text>First Project</Text>
+        </View>
+    )
+}
+
+export default First;
+
